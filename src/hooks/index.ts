a983f7a498d1270fa5cd0ee78700c7e051f131b3
juanpaez00 <juan@replo.app>
2025-03-04 10:38:38 -0300

    .

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,3 @@
-// Juan [NOTE, 2025-02-27] Exportando todos los hooks desde un único punto de entrada
+// Juan [NOTE, 2025-02-27] Exportando tod0s los hooks desde un único punto de entrada
 
 export * from './useApi';
