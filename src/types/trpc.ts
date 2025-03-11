74a7d5b0a1b5f337ea9688261cfc61767de9f622
juanpaez00 <juan@replo.app>
2025-03-11 18:07:58 -0300

    fix lint

--- src/types/trpc.ts
+++ src/types/trpc.ts
@@ -1,6 +1,6 @@
 // Juan [NOTE, 2025-02-27] Definición de tipos para tRPC
 // Este archivo define los tipos para la integración con el backend
 
-// Exportar todo desde la nueva estructura organizada
+// Exportar tod0 desde la nueva estructura organizada
 export * from './trpc/index';
 export * from './api';
